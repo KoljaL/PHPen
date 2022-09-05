53a9adcc4d6cc203903c964b1d379c58713a2f1a
Kolja <lasar@rasal.de>
2022-09-05 16:49:12 +0200

    ADD CSS fixes

--- files/_output.php
+++ files/_output.php
@@ -1,15 +1,3 @@
 <?php
 echo'<br>';
-echo date("l jS of F Y h:i:s A");
-echo'<br>';
 echo "test";
-echo'<br>';
-$cars = array(  array("Volvo",22,18),  array("BMW",15,13),  array("Saab",5,2),  array("Land Rover",17,15));
-echo'<br>';
-print_r($cars);
-echo'<br>';
-$colors = array("red", "green", "blue", "yellow");
-echo'<br>';
-foreach ($colors as $value) {
-    echo "$value <br>";
-}
